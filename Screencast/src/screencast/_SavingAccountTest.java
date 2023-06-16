70aef8744bb6d2fe0edd6ba0bb6e00b08e7440f0
Nouman Jamil <itsnoumanjamil@gmail.com>
2023-06-16 13:16:10 +0500

    Project Setup

---
+++ Screencast/src/screencast/_SavingAccountTest.java
@@ -0,0 +1,14 @@
+package screencast;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class _SavingAccountTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
